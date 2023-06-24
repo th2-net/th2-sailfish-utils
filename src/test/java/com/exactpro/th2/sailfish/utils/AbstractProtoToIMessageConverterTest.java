76cb44647f42e38137426132ee2e9e66e4e45801
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2023-06-24 15:45:11 +0400

    [th2-1156] configured com.github.hierynomus.license

--- src/test/java/com/exactpro/th2/sailfish/utils/AbstractProtoToIMessageConverterTest.java
+++ src/test/java/com/exactpro/th2/sailfish/utils/AbstractProtoToIMessageConverterTest.java
@@ -1,20 +1,26 @@
-/*
- *  Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+/**
+ * Copyright 2020-2023 Exactpro (Exactpro Systems Limited)
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package com.exactpro.th2.sailfish.utils;
+
+import com.exactpro.sf.common.messages.IMessage;
+import com.exactpro.sf.comparison.ComparatorSettings;
+import com.exactpro.sf.comparison.ComparisonResult;
+import org.junit.jupiter.api.Assertions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.exactpro.sf.comparison.ComparisonUtil.getResultCount;
 import static com.exactpro.sf.comparison.MessageComparator.compare;
@@ -23,23 +29,6 @@ import static com.exactpro.sf.scriptrunner.StatusType.CONDITIONALLY_PASSED;
 import static com.exactpro.sf.scriptrunner.StatusType.FAILED;
 import static com.exactpro.sf.scriptrunner.StatusType.PASSED;
 import static com.exactpro.th2.sailfish.utils.Messages.getSimpleFieldCountRecursive;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.Assertions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.exactpro.sf.common.messages.IMessage;
-import com.exactpro.sf.comparison.ComparatorSettings;
-import com.exactpro.sf.comparison.ComparisonResult;
-import com.exactpro.th2.common.grpc.ListValue;
-import com.exactpro.th2.common.grpc.Message;
-import com.exactpro.th2.common.grpc.MessageMetadata;
-import com.exactpro.th2.common.grpc.Value;
 
 public class AbstractProtoToIMessageConverterTest extends AbstractConverterTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractProtoToIMessageConverterTest.class);
